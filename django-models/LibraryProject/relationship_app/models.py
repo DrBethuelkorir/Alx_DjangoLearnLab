342ac06876d7e4876fe75ae04e0aadf8d61148f5
Dr.sheriff <kiptoobethuel0@gmail.com>
2025-11-05 17:20:35 +0300

    neseesary

--- django-models/LibraryProject/relationship_app/models.py
+++ django-models/LibraryProject/relationship_app/models.py
@@ -1,19 +1,31 @@
 from django.db import models
 
-class Author(models.Model):  # Capital M
+class Author(models.Model):
     name = models.CharField(max_length=200)
+    
+    def __str__(self):
+        return self.name  # Added this line
 
-class Book(models.Model):  # Capital M
+class Book(models.Model):
     title = models.CharField(max_length=200)
-    author = models.ForeignKey(Author, on_delete=models.CASCADE)  # models.CASCADE
+    author = models.ForeignKey(Author, on_delete=models.CASCADE)
+    
+    def __str__(self):
+        return self.title  # Added this line
 
-class Library(models.Model):  # Capital M
-    name = models.CharField(max_length=200)  # models.CharField with max_length
-    books = models.ManyToManyField(Book)  # ManyToManyField
+class Library(models.Model):
+    name = models.CharField(max_length=200)
+    books = models.ManyToManyField(Book)
+    
+    def __str__(self):
+        return self.name  # Added this line
 
-class Librarian(models.Model):  # Capital M
+class Librarian(models.Model):
     name = models.CharField(max_length=200)
     library = models.OneToOneField(
         Library, 
-        on_delete=models.CASCADE  # Required on_delete parameter
+        on_delete=models.CASCADE
     )
+    
+    def __str__(self):
+        return self.name  # Added this line
