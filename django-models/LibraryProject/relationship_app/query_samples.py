8dcc6f380a071cdae5fcb727fc882578a2c7393b
Dr.sheriff <kiptoobethuel0@gmail.com>
2025-11-05 22:29:41 +0300

    added

--- django-models/LibraryProject/relationship_app/query_samples.py
+++ django-models/LibraryProject/relationship_app/query_samples.py
@@ -1,15 +1,16 @@
-author = Author.objects.get(name="J.K. Rowling")
-books = Book.objects.filter(author=author)
-
 # Get a specific library
 library = Library.objects.get(name="City Central Library")
-
-# Get all books in that library
 books = library.books.all()
 
 print(f"Books in {library.name}:")
 for book in books:
     print(f"- {book.title} by {book.author.name}")
+
+
+
+library = Library.objects.get(name="City Cental Library")
+author = Author.objects.get(name="J.K. Rowling")
+books = Book.objects.filter(author=author)
 
 
 library = Library.objects.get(name="City Central Library")
