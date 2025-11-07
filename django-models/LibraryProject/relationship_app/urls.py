1f7a35d761ae661254e5fb4aa508d2fe55f6a291
Dr.sheriff <kiptoobethuel0@gmail.com>
2025-11-07 16:27:02 +0300

    m,nkljm

--- django-models/LibraryProject/relationship_app/urls.py
+++ django-models/LibraryProject/relationship_app/urls.py
@@ -1,8 +1,7 @@
 from django.urls import path
-from .views import list_books,LibraryDetailsView
+from .views import book_list, LibraryDetailView  # Correct function and class names
 
-
-urlpatterns =[
-    path('books/',views.listallbook),
-    path('library/', views.LibraryDetailsView.as_view())
+urlpatterns = [
+    path('books/', listallbook, name='book_list'),
+    path('library/<int:pk>/', LibraryDetailView.as_view(), name='library_detail'),
 ]
