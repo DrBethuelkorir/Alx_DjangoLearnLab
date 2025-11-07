59216bb0c9f1b0e9b83386af1ed4186a33907810
Dr.sheriff <kiptoobethuel0@gmail.com>
2025-11-07 16:21:02 +0300

    part 2

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -1,8 +1,7 @@
 from django.shortcuts import render
-from relationship_app.models import Book, Library
+from .models import Library, Book
 from django.views.generic import ListView
 from django.views.generic.detail import DetailView
-
 def listallbook(request):
     books = Book.objects.all()
     
@@ -12,13 +11,7 @@ def listallbook(request):
     
     return render(request, "relationship_app/list_books.html", context)
 
-class LibraryDetailView(DetailView):  # Renamed from Librarydetails to LibraryDetailView
+class LibraryDetailsView(DetailView):
     model = Library
     template_name = "relationship_app/library_detail.html"
     context_object_name = "library"
-    
-    def get_context_data(self, **kwargs):
-        context = super().get_context_data(**kwargs)
-        # Add all books from this specific library to the context
-        context['books'] = self.object.books.all()  # This gets books for the specific library
-        return context
