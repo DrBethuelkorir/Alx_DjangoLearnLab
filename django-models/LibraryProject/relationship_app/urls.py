a98afd7bf4cffe3287fbce27f141eb7159d8921a
Dr.sheriff <kiptoobethuel0@gmail.com>
2025-11-07 16:06:10 +0300

    class models views

--- django-models/LibraryProject/relationship_app/urls.py
+++ django-models/LibraryProject/relationship_app/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
+from . import views
 
 
 urlpatterns =[
-    path('books/' views.listallbook)
+    path('books/',views.listallbook),
+    path('library/', views.Librarydetails.as_view())
 ]
--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -1,11 +1,16 @@
 from django.shortcuts import render
-from relationship_app.models import Book
-
+from relationship_app.models import Book,Library
+from django.views.generic import ListView
 def listallbook(request):
     books = Book.objects.all()
     
     context = {
-        'books': books
+        'books': books,
     }
     
     return render(request, "relationship_app/list_books.html", context)
+
+class Librarydetails(ListView):
+    model = Library
+    template_name = "relationship_app/library_detail.html"
+    context_object_name = "library"
