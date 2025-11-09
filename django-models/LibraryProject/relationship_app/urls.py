5cf9dfe8939dfb3c54975fb44f4f6d7081334fcc
Dr.sheriff <kiptoobethuel0@gmail.com>
2025-11-09 05:55:54 +0300

    views.register

--- django-models/LibraryProject/relationship_app/urls.py
+++ django-models/LibraryProject/relationship_app/urls.py
@@ -1,11 +1,11 @@
 from django.contrib.auth.views import LoginView, LogoutView
 from django.urls import path
-from .views import listallbook, LibraryDetailsView, Register  # Import Register class
+from . import views  # Import the views module
 
 urlpatterns = [
-    path('books/', listallbook, name='book_list'),
-    path('library/<int:pk>/', LibraryDetailsView.as_view(), name='library_detail'),
+    path('books/', views.listallbook, name='book_list'),
+    path('library/<int:pk>/', views.LibraryDetailsView.as_view(), name='library_detail'),
     path('login/', LoginView.as_view(template_name="relationship_app/loginhtml"), name="login"),
     path('logout/', LogoutView.as_view(template_name="relationship_app/logout.html"), name="logout"),
-    path('register/', Register.as_view(), name='register'),  # Class-based view with .as_view()
+    path('register/', views.register, name='register'),  # Explicitly using views.register
 ]
